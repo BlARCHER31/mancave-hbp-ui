a6a29f6f3e0cb7f5eff59d27e20f956d3446f78e
Blake Archer <blarcher31@gmail.com>
2022-02-28 09:59:25 -0600

    about description updates and style updates

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -3,9 +3,26 @@ import cassImage from './about-images/temp-barber-photo.jpg'
 import AboutIndividual from './about-individual'
 import './about.css'
 
-const cassDescription = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quibusdam aliquid similique neque vel non iste excepturi quis porro repudiandae accusamus perspiciatis perferendis animi obcaecati ea soluta est debitis id, saepe sapiente delectus. Placeat, fugiat nisi. Alias quasi suscipit aspernatur pariatur aperiam possimus perferendis rem tenetur necessitatibus expedita eveniet nihil, minus architecto, tempore temporibus magnam sequi in optio facere fugiat.`
+const cassDescription = `Hey guys, I'm Cassidy! I'm 26 years old and I was born and raised in Southern California. 
+                        I moved to Middle Tennessee when I was 22. I have been in and around the industry since I was 
+                        born growing up in my mom's salon. I got my barber license when I was 20 years old and have been cutting 
+                        hair ever since. I specialize in straight razor fades, beard trims, and shaves. My priority is to always
+                        have you walking out confident and looking like your best you. I do this by listening to what you want and 
+                        guiding you with my expertise on how to get there. I can't wait for you to come in and see the new place. Thank you
+                        so much for tagging along with us on our journey! Now let's cut your hair and make you feel like your best self!`
 
-const oliviaDescription = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi iste quibusdam, temporibus beatae facere at fuga magni rem asperiores ea nesciunt ullam eius molestias accusamus necessitatibus tempora quod sapiente esse hic earum commodi? Dicta, architecto voluptates pariatur ea dolore asperiores numquam veritatis ex modi quod eius exercitationem, voluptatem tempora iste.`
+const oliviaDescription = `As a 3rd generation cosmetologist, I grew up in the hair and beauty industry and have 
+                          always had a love for this profession. About 5 years ago, I grew a deeper passion for men's 
+                          grooming while living in Colorado Springs working near the Peterson Air 
+                          Force base where I became inspired to learn how to perfect
+                          men's hair cuts. For 8 years now, I have been dedicated to developing a partnership with my clients to 
+                          gain a deeper understanding of what they are aiming to achieve. With this mindset, 
+                          I have been led to the next step, in my career, by offering these services as a local small 
+                          business owner of "The ManCave". I am so excited to continue this journey with every
+                          person who walks through our doors. I aim to provide only the finest haircuts, scalp massages, 
+                          hot towel & shave, and of course beard trims.  paired with a cold beverage. 
+                          I am so thankful that we are able to create this space for you to kick back, relax and walk 
+                          away feeling more confident. I look forward to your appointment in the ManCave.`
 
 const About = () => {
   return (
